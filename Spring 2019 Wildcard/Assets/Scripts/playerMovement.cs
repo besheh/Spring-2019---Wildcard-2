ce600f4ab80e092f01a12666fa6392b313d7c7fd
Unknown <kelvin.mai101@yahoo.com>
2019-04-17 16:59:51 -0700

    Basic Movement

---
+++ Spring 2019 Wildcard/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    float moveSpeed = 5f;
+    float dirX, dirY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        dirY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.position = new Vector2(transform.position.x + dirX, transform.position.y + dirY);
+    }
+}
