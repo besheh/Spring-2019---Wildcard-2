6651d9ec7e16155abb2c1ff3ede18b4ab3729c83
gabeeck <eck.gabe@gmail.com>
2019-05-08 17:49:34 -0700

    Finished basics of hacking minigame

--- Spring 2019 Wildcard/Assets/Scripts/HackThornton.cs
+++ Spring 2019 Wildcard/Assets/Scripts/HackThornton.cs
@@ -7,9 +7,9 @@ public class HackThornton : MonoBehaviour
 {
     [SerializeField] TMP_InputField usernameInput = null;
     [SerializeField] TMP_InputField passwordInput = null;
-    [SerializeField] string username = "Thornton";
+    [SerializeField] string email = null;
     [SerializeField] string password = null;
-    [SerializeField] string guessedUsername = null;
+    [SerializeField] string guessedEmail = null;
     [SerializeField] string guessedPassword = null;
 
     // Start is called before the first frame update
@@ -21,34 +21,37 @@ public class HackThornton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
-
-        if (CheckCorrect(guessedUsername, username)
-            && CheckCorrect(guessedPassword, password))
-        {
-            //Show the list of students and put the list onto the usb
-
-            
-        }
         
-            
-        
-            
     }
 
+    //checks if a given input string is correct
     bool CheckCorrect(string given, string correct)
     {
         if (given == correct)
         {
             return true;
         }
-
         return false;
     }
 
-    void TextChanged(string newText)
+    
+    public void UsernameChanged(string newText)
     {
-        float temp = float.Parse(newText);
+        guessedEmail = newText;
+    }
+
+    public void PasswordChanged(string newText)
+    {
+        guessedPassword = newText;
+    }
+
+    public void CheckLogin()
+    {
+        if (CheckCorrect(guessedEmail, email)
+            && CheckCorrect(guessedPassword, password))
+        {
+            //Show the list of students and put the list onto the usb
+            Debug.Log("I'm in");
+        }
     }
 }
