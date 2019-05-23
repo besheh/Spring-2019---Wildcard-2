01ca7af2251bec707495891e1e7a8fce8e025311
gabeeck <eck.gabe@gmail.com>
2019-05-22 18:31:50 -0700

    Another shop update

---
+++ Spring 2019 Wildcard/Assets/Scripts/ShopSystem/ItemHolder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ItemHolder : MonoBehaviour
+{
+    public TMP_Text itemName;
+    public TMP_Text itemPrice;
+    public Image itemIcon;
+    public GameObject buyButton;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Spring 2019 Wildcard/Assets/Scripts/ShopSystem/Shop.cs
+++ Spring 2019 Wildcard/Assets/Scripts/ShopSystem/Shop.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Shop : MonoBehaviour
 {
     public List<Item> itemList = new List<Item>();
+
+    public GameObject itemHolderPrefab;
+    public Transform grid;
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +19,16 @@ public class Shop : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void fillList()
+    {
+        for (int i = 0, i < itemList.Count; i++ )
+        {
+            GameObject holder = Instantiate(itemHolderPrefab, grid);
+
+
+        }
     }
 
     void BuyItem()
